2083886ab56e8f8135d22e72a2475a9d31654584
Charlies-Kvit <theivangao@gmail.com>
2024-09-25 17:34:18 +0300

    length converter ready

--- src/app.py
+++ src/app.py
@@ -1,7 +1,16 @@
 from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
-data = {"ml": 1, "cm": 10, "meter": 100, "km": 1000, "inch":}
+data = {
+    'ml': 1609,
+    'yard': 0.9144,
+    'ft': 30.48,
+    'inch': 0.0254,
+    'km': 1000,
+    'meter': 1,
+    'cm': 0.01,
+    'mm': 0.001,
+}
 
 
 @app.route('/')
@@ -13,9 +22,12 @@ def index():
 def length():
     if request.values.get('value') is not None:
         value = request.values.get('value')
-        convert_from = request.values.get('unit_form')
+        convert_from = request.values.get('unit_from')
         convert_to = request.values.get('unit_to')
-        return
+        meter_value = float(int(value) * data[convert_from])
+        answer = float(meter_value / data[convert_to])
+        message = '{} {} = {} {}'.format(value, convert_from, answer, convert_to)
+        return render_template('length.html', message=message)
     else:
         return render_template('length.html', message=None)
 
